54e01c0dff6e7846c058ceea53af84680a2adc4b
Rioba-Ian <riobafelix@gmail.com>
2024-01-01 12:46:27 +0300

    tailwind css config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+ content: [],
+ theme: {
+  extend: {
+   colors: {
+    "very-dark-grey": "hsl(0,0%,17%)",
+    "dark-grey": "hsl(0,0%,59%)",
+   },
+  },
+ },
+ plugins: [],
+};
