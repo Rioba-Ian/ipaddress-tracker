2cf6c075eb5e45fa1819f1d7f6e9c1190046abbc
RiobaIan <ian.rioba@globallinkplus.com>
2024-01-02 22:41:13 +0300

    add state variable to track position

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,19 +1,25 @@
+import {LatLngExpression} from "leaflet";
+import {useState} from "react";
 import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
 
 export default function MainContent() {
+ const [position, setPosition] = useState<LatLngExpression | null>([
+  -1.286389, 36.817223,
+ ]);
+
  return (
   <div id="map" className="w-full">
    <MapContainer
-    center={[52.505, -0.1]}
-    className="h-[80vmin]"
+    center={position}
     zoom={13}
+    className="h-[100vh]"
     scrollWheelZoom={false}
    >
     <TileLayer
      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker position={[51.505, -0.09]}>
+    <Marker position={position}>
      <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
      </Popup>
