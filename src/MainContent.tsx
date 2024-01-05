aa48bf05e4371d888f63f68de561409524fcef2b
Rioba-Ian <riobafelix@gmail.com>
2024-01-05 20:14:37 +0300

    track user and show location

--- src/MainContent.tsx
+++ src/MainContent.tsx
@@ -1,17 +1,44 @@
 import {LatLngExpression} from "leaflet";
-import {useState} from "react";
-import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
+import {useEffect, useState} from "react";
+import {
+ MapContainer,
+ TileLayer,
+ Marker,
+ Popup,
+ useMapEvents,
+} from "react-leaflet";
 import {useAtom} from "jotai";
 import apiDataAtom from "./lib/apiDataAtom";
 
+function LocationMarker() {
+ const [position, setPosition] = useState<LatLngExpression | null>(null);
+ const map = useMapEvents({
+  locationfound(e) {
+   setPosition([e.latlng.lat, e.latlng.lng]);
+   map.flyTo([e.latlng.lat, e.latlng.lng], map.getZoom());
+  },
+ });
+
+ useEffect(() => {
+  map.locate();
+ }, []);
+
+ return position === null ? null : (
+  <Marker position={position}>
+   <Popup>You are here</Popup>
+  </Marker>
+ );
+}
+
 export default function MainContent() {
  const [data] = useAtom(apiDataAtom);
- const [position] = useState<LatLngExpression>([-1.286389, 36.817223]);
 
  return (
   <div id="map" className="w-full">
    <MapContainer
-    center={position}
+    center={
+     data ? [data.location.lat, data.location.lng] : [-1.286389, 36.817223]
+    }
     zoom={13}
     className="h-[100vh]"
     scrollWheelZoom={false}
@@ -20,13 +47,7 @@ export default function MainContent() {
      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
     />
-    <Marker
-     position={data ? [data.location.lat, data?.location.lng] : position}
-    >
-     <Popup className="text-very-dark-grey">
-      A pretty CSS3 popup. <br /> Easily customizable.
-     </Popup>
-    </Marker>
+    <LocationMarker />
    </MapContainer>
   </div>
  );
